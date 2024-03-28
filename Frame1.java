d75c3cba9de10d38fc0b8c1292bc7cf9e83704ef
venkatesh1545 <119730058+venkatesh1545@users.noreply.github.com>
2024-03-29 02:02:25 +0530

    Add files via upload

---
+++ Frame1.java
@@ -0,0 +1,14 @@
+package GUI_JAVA;
+
+import javax.swing.JFrame;
+
+public class Frame1 extends JFrame {
+    Frame1() {
+        setTitle("Frame 1");
+        setSize(500, 500);
+        setLayout(null);
+        
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        setVisible(true);
+    }
+}
